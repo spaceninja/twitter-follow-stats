e5bc61fac24d2788c3946d2496c3d51a56a2c624
Scott Vandehey <fake-scott@spaceninja.com>
2019-04-14 15:43:12 -0700

    bugfix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app" class=".app">
+  <div id="app" class="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Twitter Follow Stats" />
   </div>
 </template>
 
@@ -17,8 +17,10 @@ export default {
 </script>
 
 <style lang="scss">
+$color-text-default: #2c3e50;
+
 .app {
-  color: #2c3e50;
+  color: $color-text-default;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
